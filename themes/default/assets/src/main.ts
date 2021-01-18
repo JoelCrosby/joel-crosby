b5a3cf3a7aeb4929238a216a337850db07639efc
Joel Crosby <joelcrosby@live.co.uk>
2021-01-18 16:51:41 +0000

    add dark mode toggle

--- themes/default/assets/src/main.ts
+++ themes/default/assets/src/main.ts
@@ -1,15 +1,45 @@
-const currentTheme = localStorage.getItem('theme');
-
-if (currentTheme) {
-  document.documentElement.setAttribute('data-theme', currentTheme);
-}
-
-function switchTheme(isDark: boolean) {
-  if (isDark) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-    localStorage.setItem('theme', 'dark');
-  } else {
-    document.documentElement.setAttribute('data-theme', 'light');
-    localStorage.setItem('theme', 'light');
+const getPreferScheme = (scheme: 'dark' | 'light') => {
+  if (!window.matchMedia) {
+    return null;
   }
-}
+
+  return window.matchMedia(`(prefers-color-scheme: ${scheme})`)?.matches;
+};
+
+const init = () => {
+  const localTheme = localStorage.getItem('theme');
+  const userPrefersDark = getPreferScheme('dark');
+
+  const currentTheme = localStorage.getItem('theme');
+  const toggleSwitch: HTMLInputElement | null = document.querySelector(
+    '.theme-switch input[type="checkbox"]'
+  );
+
+  if (!toggleSwitch) {
+    return;
+  }
+
+  toggleSwitch.checked =
+    (localTheme === 'dark' || userPrefersDark === true) &&
+    (!localTheme || localTheme !== 'light');
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  }
+
+  const switchTheme = (e: Event) => {
+    const toggle = e.target as HTMLInputElement;
+
+    if (toggle.checked) {
+      document.documentElement.setAttribute('data-theme', 'dark');
+      localStorage.setItem('theme', 'dark');
+    } else {
+      document.documentElement.setAttribute('data-theme', 'light');
+      localStorage.setItem('theme', 'light');
+    }
+  };
+
+  toggleSwitch.addEventListener('change', switchTheme, false);
+};
+
+init();
